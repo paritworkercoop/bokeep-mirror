04873bea83793b71b885c0cac1602ddd1043bf4f
Mark Jenkins <mark@parit.ca>
2008-07-08 23:14:29 +0000

    scripts renamed to not have .py extension
    --HG--
    rename : bo-keep.py => bo-keep
    rename : bo-keep-book-add.py => bo-keep-book-add
    rename : bo-keep-book-remove.py => bo-keep-book-remove
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%4019

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='Bo-Keep',
       version='0.1',
-      scripts=['bo-keep.py', 'bo-keep-book-add.py', 'bo-keep-book-remove.py'],
+      scripts=['bo-keep', 'bo-keep-book-add', 'bo-keep-book-remove'],
       packages=['bokeep', 'bokeep.backend_modules', 'bokeep.gui' ],
       package_dir={'bokeep': 'src/bokeep'},
       package_data={'bokeep': ['gui/glade/bokeep_main_window.glade'] },
