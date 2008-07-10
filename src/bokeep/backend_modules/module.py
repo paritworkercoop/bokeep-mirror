b7092b5f877394fea0b55a6cf33f2efd7320b61f
Mark Jenkins <mark@parit.ca>
2008-07-10 20:21:02 +0000

    initial work on backend module code started
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%4037

--- src/bokeep/backend_modules/module.py
+++ src/bokeep/backend_modules/module.py
@@ -1,5 +1,31 @@
 from persistent import Persistent
 
+from bokeep.util import \
+    ChangeMessageRecievingThread, EntityChangeManager, entitymod
+
+class BackendChangeManager(EntityChangeManager):
+    def __init__(self, running_thread, entity_identifier):
+        EntityChangeManager.__init__(self, running_thread, entity_identifier)
+        self.transaction_dirty = False
+
+class BackendChangeThread(ChangeMessageRecievingThread):
+    
+    @entitymod
+    def transaction_dirty(self, entity_identifier):
+        pass
+
+    def get_entity_from_identifier(self, entity_identifier):
+        pass
+
+    def handle_entity_change(self, entity):
+        pass
+
 class BackendModule(Persistent):
-    pass
+    def __init__(self, database):
+        Persistent.__init__(self)
+        self._v_change_thread = BackendChangeThread()
+        self._v_change_thread.run()
+
+    def transaction_dirty(self, entity_identifier):
+        pass
 
