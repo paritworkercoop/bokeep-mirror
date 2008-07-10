d976adb11cf5061c54ab1c7e5a30a96158c6a34e
Mark Jenkins <mark@parit.ca>
2008-07-10 06:34:14 +0000

    initial work to generisize message passing thread stuff
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%4030

--- src/bokeep/util.py
+++ src/bokeep/util.py
@@ -1,8 +1,54 @@
+# python standard library
+from threading import Thread, Condition
+
+# ZODB
 import transaction
 
+# simplifies common ussage of transaction.get().commit()
 def ends_with_commit(dec_function):
     def ret_func(*args, **kargs):
         return_value = dec_function(*args, **kargs)
         transaction.get().commit()
         return return_value
     return ret_func
+
+
+# Message handling thread stuff
+
+class Message(object):
+    pass
+
+class ThreadEndMessage(Message):
+    pass
+
+def changelock(dec_function):
+    def ret_function(self, *args):
+        self.change_availible.acquire()
+        return_value = dec_function(self, *args)
+        self.change_availible.release()
+        return return_value
+    return ret_function
+
+def waitlistmodify(dec_function):
+    @changelock
+    def ret_function(self, *args):
+        return_value = dec_function(self, *args)
+        self.change_availible.notify()
+        return return_value
+    return ret_function
+
+class MessageRecievingThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.message_wait_list = []
+        self.change_availible = Condition()
+
+    @waitlistmodify
+    def request_thread_end(self):
+        self.trans_wait_list.append( ThreadEndMessage() )
+
+    def end_thread_and_join(self):
+        self.request_thread_end()
+        self.join()
+    
+        
