54732cc5fd8732e9d3bed0aef9166dc70ab71faf
Mark Jenkins <mark@parit.ca>
2008-07-10 21:46:32 +0000

    more work on module implementation
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%4039

--- src/bokeep/backend_modules/module.py
+++ src/bokeep/backend_modules/module.py
@@ -6,7 +6,7 @@ from bokeep.util import \
 class BackendChangeManager(EntityChangeManager):
     def __init__(self, running_thread, entity_identifier):
         EntityChangeManager.__init__(self, running_thread, entity_identifier)
-        self.transaction_dirty = False
+        self.transaction_dirty_count = 0
 
 class BackendChangeThread(ChangeMessageRecievingThread):
     
@@ -21,11 +21,38 @@ class BackendChangeThread(ChangeMessageRecievingThread):
         pass
 
 class BackendModule(Persistent):
-    def __init__(self, database):
+    def __init__(self):
         Persistent.__init__(self)
-        self._v_change_thread = BackendChangeThread()
-        self._v_change_thread.run()
+        self.dirty_transaction_set = {}
+        self.front_end_to_back_id = {}
 
-    def transaction_dirty(self, entity_identifier):
-        pass
+    def set_transaction_dirty_count(self, entity_identifier, count):
+        assert( count >= 0 )
+        self.dirty_transaction_set[entity_identifier] = count
 
+    def mark_transaction_dirty(self, entity_identifier):
+        if entity_identifier in self.dirty_transaction_set:
+            self.dirty_transaction_set[entity_identifier] += 1
+        else:
+            self.dirty_transaction_set[entity_identifier] = 1
+
+    def decrement_transaction_dirty_count(self, entity_identifier, count=None):
+        assert( entity_identifier in self.dirty_transaction_set )
+        assert( self.dirty_transaction_set[entity_identifier] > 0 )
+        assert( count > 0 )
+        if count == None:
+            self.dirty_transaction_set[entity_identifier] = 0
+        else:
+            self.dirty_transaction_set[entity_identifier] -= count
+        
+    def set_backend_transaction_identifier(
+        self, entity_identifier, backend_identifier):
+        self.front_end_to_back_id[entity_identifier] = backend_identifier
+
+    def get_backend_transaction_identifier(self, entity_identifier):
+        if entity_identifier in self.front_end_to_back_id:
+            self.front_end_to_back_id[entity_identifier] = backend_identifier
+        else:
+            return None
+
+    
