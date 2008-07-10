9d841bc181cf7c67ae6e5a90517b1b29fce27191
Mark Jenkins <mark@parit.ca>
2008-07-10 09:25:11 +0000

    removed confusing, archaic implementation of change/delta management
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%4034

--- src/bokeep/util.py
+++ src/bokeep/util.py
@@ -118,8 +118,6 @@ class MessageRecievingThread(Thread):
         pass
 
 
-PRIMARY_DELTA, SECONDARY_DELTA, ENTITY = range(3)
-
 class EntityChangeMessage(ThreadCallbackMessage):
     def __init__(self, running_thread, entity_identifier):
         ThreadCallbackMessage.__init__(running_thread)
@@ -134,60 +132,53 @@ class EntityChangeEnd(EntityChangeMessage):
 def entitymod(dec_function):
     @changelock
     def ret_function(self, *args, **kargs):
-        delta = self.change_manager_lookup_dict[
-            (args[0], PRIMARY_DELTA) ]
-        return_value = dec_function(self, delta, *args, **kargs)
-        if not delta in self.message_wait_list:
-            self.message_wait_list.append(delta)
-            self.change_availible.notify()
-        return return_value
+        changes = self.waiting_changes[args[0]]
+        return_value = dec_function(self, changes, *args, **kargs)
+        if not args[0] in self.wait_list_change_index:
+            self.message_wait_list.append(changes)
+            self.wait_list_change_index[args[0]] = \
+                len(self.message_wait_list) - 1
+        self.change_availible.notify()
     return ret_function
 
 class ChangeMessageRecievingThread(MessageRecievingThread):
     def __init__(self):
         MessageReceivingThread.__init__(self)
-        self.change_manager_lookup_dict = {}
+        self.changes_being_processed = {}
+        self.waiting_changes = {}
+        self.entity_lookup_dict = {}
+        self.wait_list_change_index = {}
+
 
     def wait_list_switched(active_message_list, empty_list):
         """Called right after the message lists are switched with the two
         lists, but before the lock is given up.
         """
-        for delta in active_message_list:
-            if isinstance(delta, EntityChangeManager):
-                delta_keys = [ (delta.entity_identifier, x)
-                               for x in (PRIMARY_DELTA, SECONDARY_DELTA) ]
-                    
-                secondary_delta = self.change_manager_lookup_dict[
-                    delta_keys[SECONDARY_DELTA] ]
-                self.change_manager_lookup_dict[
-                    delta_keys[SECONDARY_DELTA] ] = \
-                    self.change_manager_lookup_dict[delta_keys[PRIMARY_DELTA] ]
-                self.change_manager_lookup_dict[ delta_keys[PRIMARY_DELTA] ] = \
-                    secondary_delta
+        temp = self.changes_being_processed
+        self.changes_being_processed  = self.waiting_changes
+        self.waiting_changes = temp
 
     @changelock
     def add_change_tracker(self, entity_identifier):
-        assert( (entity_identifier, PRIMARY_DELTA) not in
-                self.change_manager_lookup_dict )
-        for x in (PRIMARY_DELTA, SECONDARY_DELTA):
-            self.change_manager_lookup_dict[(entity_identifier, x)] = \
-            EntityChangeManager(self, entity_identifier)
+        assert( entity_identifier not in self.changes_being_processed)
+        for dictionary in (self.changes_being_processed, self.waiting_changes):
+            dictionary[entity_identifier] = \
+                EntityChangeManager( self, entity_identifier)
 
     @waitlistmodify
     def remove_change_tracker(self, entity_identifier):
-        assert( (entity_identifier, PRIMARY_DELTA) in
-                self.change_manager_lookup_dict )
+        assert( entity_identifier in self.changes_being_processed )
         self.message_wait_list.append(
             EntityChangeEnd(self, entity_identifier) )
 
     @changelock
     def get_entity_for_delta(self, delta):
-        entity_key = (delta.entity_identifier, ENTITY)
-        if not entity_key in self.change_manager_lookup_dict:
+        entity_key = delta.entity_identifier
+        if not entity_key in entity_lookup_dict:
             entity = self.get_entity_from_identifier(delta.entity_identifier)
-            self.change_manager_lookup_dict[entity_key] = entity
+            self.entity_lookup_dict[entity_key] = entity
         else:
-            entity = self.change_manager_lookup_dict[entity_key]
+            entity = self.entity_lookup_dict[entity_key]
         return entity
 
     def handle_entity_change_message(self, change_message):
@@ -195,6 +186,13 @@ class ChangeMessageRecievingThread(MessageRecievingThread):
     
     @changelock
     def remove_delta(self, message):
-        for x in (PRIMARY_DELTA, SECONDARY_DELTA, TRANSACTION):
-            del self.change_manager_lookup_dict[(message.entity_identifier, x)]
+        entity_key = message.entity_identifier
+        for dictionary in (self.changes_being_processed, self.waiting_changes):
+            del dictionary[entity_key]
+        if entity_key in self.entity_lookup_dict:
+            del self.entity_lookup_dict[entity_key]
         
+    @changelock
+    def message_block_end(self):
+        MessageRecievingThread.message_block_end(self)
+        self.wait_list_change_index.clear()
