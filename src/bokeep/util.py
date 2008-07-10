442ef439a70aef8a820812abd3605de36fcd6aa3
Mark Jenkins <mark@parit.ca>
2008-07-10 07:08:26 +0000

    genresization of message passing advanced further along
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%4031

--- src/bokeep/util.py
+++ src/bokeep/util.py
@@ -18,8 +18,15 @@ def ends_with_commit(dec_function):
 class Message(object):
     pass
 
-class ThreadEndMessage(Message):
-    pass
+class ThreadAccessingMessage(Message):
+    def __init__(self, running_thread):
+        Message.__init__(self)
+        self.running_thread = running_thread    
+
+class ThreadEndMessage(ThreadAccessingMessage):
+    def handle_message(self):
+        self.runing_thread.set_stop_running_flag()
+        
 
 def changelock(dec_function):
     def ret_function(self, *args):
@@ -42,6 +49,7 @@ class MessageRecievingThread(Thread):
         Thread.__init__(self)
         self.message_wait_list = []
         self.change_availible = Condition()
+        self.__continue_running = True
 
     @waitlistmodify
     def request_thread_end(self):
@@ -51,4 +59,54 @@ class MessageRecievingThread(Thread):
         self.request_thread_end()
         self.join()
     
-        
+    @changelock
+    def continue_running(self):
+        return self.__continue_running
+
+    @changelock
+    def set_stop_running_flag(self):
+        self.__continue_running = False
+
+    def run(self):       
+        empty_list = []
+
+        while self.continue_running():
+            self.change_availible.acquire()
+            if len(self.message_wait_list) == 0:
+                self.change_availible.wait()
+            assert( len(self.message_wait_list) > 0 )
+            
+            # lock still acquired [.acquire()] or reacquired [.wait()] here
+            # we quickly switch out the waiting list of messages
+            # with the empty list of messages
+            active_message_list = self.message_wait_list
+            self.message_wait_list = empty_list
+            self.wait_lists_switched(active_message_list, empty_list)
+            self.change_availible.release()
+            # lock released
+            
+            self.message_block_begin()
+
+            # deal with everything in trans_to_handle, reverse to reflect
+            # actual order of entry
+            active_message_list.reverse()
+            while len(active_message_list) > 0:
+                messsage = active_message_list.pop()
+                message.handle_message()
+
+            # active_message_list now empty
+            empty_list = active_message_list
+
+            self.message_block_end()
+
+    def wait_lists_switched(active_message_list, empty_list):
+        """Called right after the message lists are switched with the two
+        lists, but before the lock is given up.
+        """
+        pass
+
+    def message_block_begin():
+        pass
+
+    def message_block_end():
+        pass
