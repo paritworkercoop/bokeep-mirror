b72a9614fb383f39f8bc4b894ac10b2965b82d08
Mark Jenkins <mark@parit.ca>
2008-07-20 07:48:46 +0000

    new generic messaging and entity change tracking, implemented
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%4045

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -47,46 +47,50 @@ def after_commit_read(book, trans_key):
 after_commit_read(test_book_1, trans_key)
 
 @ends_with_commit
-def use_commit_thread(book, trans_key):
-
-    trans_thread = new_transaction_committing_thread(books)
-    trans_thread.add_transaction(book.book_name, trans_key)
+def use_commit_thread(trans_thread, book, trans_key):
+    trans_thread.add_change_tracker_block((book.book_name, trans_key))
     trans_thread.mod_transaction_attr(
-        book.book_name, trans_key, 'data', 'fuck')
-    trans_thread.end_trans_thread()
+        (book.book_name, trans_key), 'data', 'fuck')
+    trans_thread.remove_change_tracker((book.book_name, trans_key))
+    trans_thread.add_change_tracker_block((book.book_name, trans_key))
+    trans_thread.remove_change_tracker((book.book_name, trans_key))
 
-use_commit_thread(test_book_1, trans_key)
+trans_thread = new_transaction_committing_thread(books)
+use_commit_thread(trans_thread, test_book_1, trans_key)
 
 trans = test_book_1.get_transaction(trans_key)
 assert( trans.data == "fuck" )
 
 @ends_with_commit
-def use_commit_thread_again(books, book, trans_key):
-
-    trans_thread = new_transaction_committing_thread(books)
-    trans_thread.add_transaction(book.book_name, trans_key)
+def use_commit_thread_again(trans_thread, books, book, trans_key):
+    trans_thread.add_change_tracker_block((book.book_name, trans_key))
     trans_thread.mod_transaction_with_func(
-        book.book_name, trans_key, 'reset_data', (), {} )
+        (book.book_name, trans_key), 'reset_data', (), {} )
     trans_thread.mod_transaction_with_func(
-        book.book_name, trans_key, 'append_data',
+        (book.book_name, trans_key), 'append_data',
         (' shoot',), {} )
-    trans_thread.end_trans_thread()
+    trans_thread.remove_change_tracker((book.book_name, trans_key))
+    trans_thread.add_change_tracker_block((book.book_name, trans_key))
+    trans_thread.remove_change_tracker((book.book_name, trans_key))
 
-use_commit_thread_again(books, test_book_1, trans_key)
+use_commit_thread_again(trans_thread, books, test_book_1, trans_key)
 
 trans = test_book_1.get_transaction(trans_key)
 assert( trans.data == "blah shoot" )
 
 @ends_with_commit
 def use_mirror_class(books, book, trans_key):
-    trans_thread = new_transaction_committing_thread(books)
-    trans_thread.add_transaction(book.book_name, trans_key)
+    trans_thread.add_change_tracker_block((book.book_name, trans_key))
     mirror = TransactionMirror( book.book_name, trans_key, trans_thread)
     mirror.data = "juice"
     mirror.append_data(" maker")
-    trans_thread.end_trans_thread()
+    trans_thread.remove_change_tracker((book.book_name, trans_key))
+    trans_thread.add_change_tracker_block((book.book_name, trans_key))
+    trans_thread.remove_change_tracker((book.book_name, trans_key))
 
 use_mirror_class(books, test_book_1, trans_key)
+
+trans_thread.end_thread_and_join()
 
 trans = test_book_1.get_transaction(trans_key)
 assert( trans.data == "juice maker" )
