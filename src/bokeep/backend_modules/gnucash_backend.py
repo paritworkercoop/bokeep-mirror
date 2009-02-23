91ccf3663806973975d87faefc56a332741e174c
Mark Jenkins <mark@parit.ca>
2009-02-23 22:30:00 +0000

    Made an initial stab at a gnucash backend module.
    NOTE! This is only tested to compile. It has not been tested to actually
    work.
    
    Also, the backend does not return a gnucash guid for created
    transactions (as it should), and this means that
    remove_backend_transaction also doesn't do any thing.
    
    Additional note, the gnucash_file attribute has to be set to make this
    useful. The ability to set this and any other arbitrary backend
    attribute should be added to the script bo-keep-module-control (hint,
    use the setattr function)
    
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40274

---
+++ src/bokeep/backend_modules/gnucash_backend.py
@@ -0,0 +1,82 @@
+# python imports
+from decimal import Decimal 
+
+#gnucash imports
+from gnucash import Session, GncNumeric, Split, Transaction
+
+# bokeep imports
+from module import BackendModule
+from bokeep.book_transaction import \
+    BoKeepTransactionNotMappableToFinancialTransaction
+from bokeep.util import attribute_or_blank
+
+ZERO = Decimal(0)
+
+def get_amount_from_trans_line(trans_line):
+    return GncNumeric( "%.2f" % trans_line.amount )
+
+def account_from_path(top_account, account_path):
+    account, account_path = account_path[0], account_path[1:]
+    account = top_account.lookup_by_name(account)
+    if len(account_path) > 0 :
+        return account_from_path(account, account_path)
+    else:
+        return account
+
+def get_account_from_trans_line(top_level_account, trans_line):
+    assert( hasattr(trans_line, "account_spec") )
+    return account_from_path(top_level_account, trans_line.account_spec)
+
+class GnuCash(BackendModule):
+    def __init__(self):
+        BackendModule.__init__(self)
+        self.gnucash_file = ""
+        self._v_book_open = False
+        self.count = 0
+
+    def openbook_if_not_open(self):
+        if not hasattr(self, '_v_book_open') or not self._v_book_open:
+            self._v_session = Session("file:" + self.gnucash_file, False)
+            self._v_book_open = True
+        return True
+
+    def remove_backend_transaction(self, backend_ident):
+        if self.openbook_if_not_open():
+            pass
+        
+    def create_backend_transaction(self, fin_trans):
+        if self.openbook_if_not_open():
+            description = attribute_or_blank(fin_trans, "description")
+            chequenum = attribute_or_blank(fin_trans, "chequenum")
+            lines = [ Split(
+                    self._v_session.book,
+                    get_amount_from_trans_line(trans_line),
+                    get_account_from_trans_line(
+                        self._v_session.book.get_root_account(),
+                        trans_line) )
+                      for trans_line in fin_trans.lines ]
+            commodtable = GncCommodityTable(
+                instance=gnc_commodity_table_get_table(
+                    self._v_session.book.get_instance()) )
+            CAD = gnc_commodity_table_lookup(
+                commodtable.get_instance(), "ISO4217","CAD")
+
+            trans = Transaction(self._v_session.book, CAD, lines)
+            trans.SetDescription(
+                attribute_or_blank(fin_trans, "description") )
+            trans.SetNum(
+                attribute_or_blank(fin_trans, "chequenum") )
+
+            for i, split_line in enumerate(lines):
+                split_line.SetMemo( attribute_or_blank(fin_trans.lines[i],
+                                                       "comment" ) )
+            return_value = self.count
+            self.count+=1
+            return return_value
+
+    def save(self):
+        if self.openbook_if_not_open():
+            self._v_session.save()
+
+def get_module_class():
+    return GnuCash
