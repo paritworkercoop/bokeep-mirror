64ad81ec7881e3d17f6f7ae2a661605ac83c84f4
Mark Jenkins <mark@parit.ca>
2009-03-15 06:07:43 +0000

    finished fixing support in backend module, used actual api of gnucash module, not the "fake" api that I had copied from the cashout app. This is the first time the gnucash_backend module has actually worked
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40282

--- src/bokeep/backend_modules/gnucash_backend.py
+++ src/bokeep/backend_modules/gnucash_backend.py
@@ -9,11 +9,62 @@ from bokeep.util import attribute_or_blank
 
 ZERO = Decimal(0)
 
-def get_amount_from_trans_line(trans_line):
-    #gnucash imports
+def gnc_numeric_from_string(numeric_string):
     from gnucash import GncNumeric
+    from gnucash.gnucash_core_c import string_to_gnc_numeric
+    numeric_string = numeric_string.strip() # murder whitespace
 
-    return GncNumeric( "%.2f" % trans_line.amount )
+    if len(numeric_string) > 0:
+        # if there is a decimal point get rid of it
+        if '.' in numeric_string:
+            # get the parts left and right of the decimal point
+            left_of_decimal, right_of_decimal = numeric_string.split('.')
+
+            negate = ''
+
+            if len(left_of_decimal) > 0:
+                if left_of_decimal[0] == '-':
+                    negate = '-'
+                    left_of_decimal = left_of_decimal[1:]
+
+                left_of_decimal = int(left_of_decimal)
+                if left_of_decimal == 0:
+                    left_of_decimal = ''
+                else:
+                    left_of_decimal = str(left_of_decimal)
+
+
+
+            # the string now consists of the left part, right part,
+            # /1 and and apropriate number of 0's
+            numeric_string = '%s%s%s/1%s' % (
+                negate,
+                left_of_decimal,
+                right_of_decimal,
+                # put len(right_of_decimal) 0's in the denominator,
+                # eg 10.1 -> 101/10,
+                # len(right_of_decimal) == 1, (one zero)
+                # eg 10.10 -> 1010/100,
+                # len(right_of_decimal) == 2, (two zeros)
+                # eg 10.100 -> 10100/1000
+                # len(right_of_decimal) == 3, (three zeros)
+                '0'*len(right_of_decimal) ) 
+    else:
+        numeric_string = '0/1'
+
+    if '/' not in numeric_string:
+        numeric_string += '/1'
+
+    gncnumeric = GncNumeric()
+    convert_success = string_to_gnc_numeric(
+        numeric_string,
+        gncnumeric.get_instance()
+        )
+    assert( convert_success )
+    return gncnumeric
+
+def get_amount_from_trans_line(trans_line):
+    return gnc_numeric_from_string(str(trans_line.amount))
 
 def account_from_path(top_account, account_path):
     account, account_path = account_path[0], account_path[1:]
@@ -26,6 +77,16 @@ def account_from_path(top_account, account_path):
 def get_account_from_trans_line(top_level_account, trans_line):
     assert( hasattr(trans_line, "account_spec") )
     return account_from_path(top_level_account, trans_line.account_spec)
+
+def make_new_split(book, amount, account, trans):
+    from gnucash import Split
+    return_value = Split( book )
+    return_value.SetValue(amount)
+    return_value.SetAmount(amount)
+    return_value.SetAccount(account)
+    return_value.SetParent(trans)
+    return return_value
+
 
 class GnuCash(BackendModule):
     def __init__(self):
@@ -49,29 +110,39 @@ class GnuCash(BackendModule):
             pass
         
     def create_backend_transaction(self, fin_trans):
-        from gnucash import Transaction, Split, GncNumeric
-
+        from gnucash import Transaction, GncCommodityTable
+        from gnucash.gnucash_core_c import \
+            gnc_commodity_table_get_table, gnc_commodity_table_lookup
+        
         if self.openbook_if_not_open():
             description = attribute_or_blank(fin_trans, "description")
             chequenum = attribute_or_blank(fin_trans, "chequenum")
-            lines = [ Split(
+            # important, don't do anything to transaction until splits are
+            # added
+            trans = Transaction(self._v_session.book)
+
+            # create a list of GnuCash splits, set the amount, account,
+            # and parent them with the Transaction
+            lines = [ make_new_split(
                     self._v_session.book,
                     get_amount_from_trans_line(trans_line),
                     get_account_from_trans_line(
                         self._v_session.book.get_root_account(),
-                        trans_line) )
+                        trans_line),
+                    trans )
                       for trans_line in fin_trans.lines ]
+            
             commodtable = GncCommodityTable(
                 instance=gnc_commodity_table_get_table(
                     self._v_session.book.get_instance()) )
             CAD = gnc_commodity_table_lookup(
                 commodtable.get_instance(), "ISO4217","CAD")
-
-            trans = Transaction(self._v_session.book, CAD, lines)
+            
+            trans.SetCurrency(CAD)
             trans.SetDescription(
                 attribute_or_blank(fin_trans, "description") )
             trans.SetNum(
-                attribute_or_blank(fin_trans, "chequenum") )
+                str( attribute_or_blank(fin_trans, "chequenum") ) )
 
             for i, split_line in enumerate(lines):
                 split_line.SetMemo( attribute_or_blank(fin_trans.lines[i],
