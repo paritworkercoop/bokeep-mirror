edbb105a9dc747c98d57c37ab0b6970bdb5a3398
Mark Jenkins <mark@parit.ca>
2009-03-20 13:49:29 +0000

    fixed setup.py to use current names for scripts
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40287

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='Bo-Keep',
       version='0.1',
       scripts=['bo-keep', 'bo-keep-book-add', 'bo-keep-book-remove',
-               'bo-keep-module-control'],
+               'bo_keep_module_control', 'bo_keep_payroll.py'],
       packages=['bokeep', 'bokeep.backend_modules', 'bokeep.gui' ],
       package_dir={'bokeep': 'src/bokeep'},
       package_data={'bokeep': ['gui/glade/bokeep_main_window.glade'] },
