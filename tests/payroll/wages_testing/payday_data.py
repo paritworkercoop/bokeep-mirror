79b1a0d08744e00d3c94c878ca3cbd1a9e049c96
Jamie Campbell <jamie@parit.ca>
2009-05-04 05:28:45 +0000

    updated the first paystub file to reflect april 1,2009 deduction changes, also added config and data as they were somehow not added before
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40321

---
+++ tests/payroll/wages_testing/payday_data.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+paydate = date(2009, 04, 22)
+period_start = date(2009, 04, 06)
+period_end = date(2009, 04, 19)
+
+payday_serial = 1
+chequenum_start = 239
+emp_list = [
+    dict( name="george costanza",
+          rate=7.6,
+#initialization hours
+          hours=84.5
+          ),
+
+    dict( name="susie",
+          rate=7.6,
+#initialization hours
+          hours=51.5
+          ),
+]
