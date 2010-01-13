ff03d77a77688ba079fad128b90892d0dd2f0158
Mark Jenkins <mark@parit.ca>
2010-01-13 07:29:33 +0000

    started to port over old bo-keep tests.
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40434

--- setup.py
+++ setup.py
@@ -1,9 +1,50 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from glob import glob
+from os.path import splitext, basename
+import os
+import os.path
+import sys
 
+from distutils.core import setup, Command
+from unittest import TextTestRunner, TestLoader
+
+TESTS_DIR = 'tests'
+
+BOKEEP_SRC_DIR = 'src'
+PACKAGE_NAME = 'bokeep'
+
+class TestCommand(Command):
+    # modified from the code at this article:
+    #http://da44en.wordpress.com/2002/11/22/using-distutils/
+
+    user_options = [ ]
+    
+    def initialize_options(self):
+        self._dir = os.getcwd()
+        
+    def finalize_options(self):
+        pass
+
+    def generate_test_files(self):
+        return ( 
+            "%s.%s" % ( TESTS_DIR, splitext(basename(t))[0])
+            for t in glob(os.path.join(self._dir, TESTS_DIR, '*.py'))
+            if not t.endswith('__init__.py')
+            )
+    
+    def run(self):
+        sys.path.insert(0, os.path.join(self._dir, BOKEEP_SRC_DIR) )
+        tests = TestLoader().loadTestsFromNames( self.generate_test_files() )
+        t = TextTestRunner(verbosity = 1)
+        t.run(tests)
+
+# if you're thinking of changing name='Bo-Keep' to name=PACKAGE_NAME, think
+# twice, what's going to happen when you do a setup.py install on top of
+# an old installation?
 setup(name='Bo-Keep',
-      version='0.4',
+      version='0.4.1',
+      cmdclass = { 'test': TestCommand },
       scripts=['bo-keep', 'bo-keep-book-add', 'bo-keep-book-remove',
                'bo_keep_module_control', 'bo_keep_payroll.py',
                'initialize_accounting_system.sh', 'edit_bokeep_payroll.sh',
@@ -11,10 +52,11 @@ setup(name='Bo-Keep',
                'bo_keep_fix_bug_in_v0.3_payroll_database.py',
                'bo_keep_payroll_csv_dump.py'
                ],
-      packages=['bokeep', 'bokeep.backend_modules', 'bokeep.gui',
+      packages=[PACKAGE_NAME, 'bokeep.backend_modules', 'bokeep.gui',
                 'bokeep.modules', 'bokeep.modules.payroll'],
-      package_dir={'bokeep': 'src/bokeep'},
-      package_data={'bokeep': ['gui/glade/bokeep_main_window.glade'] },
+      package_dir={PACKAGE_NAME:
+                       os.path.join(BOKEEP_SRC_DIR, PACKAGE_NAME) },
+      package_data={PACKAGE_NAME: ['gui/glade/bokeep_main_window.glade'] },
       data_files=[('share/bokeep_initialization',
                    ['.bo-keep.cfg',
                     'examples/bokeep_configuration/books.conf'] ),
--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python
+from unittest import TestCase, main
 
 from bokeep.book import BoKeepBookSet
 from bokeep.book_transaction import \
     Transaction, new_transaction_committing_thread, TransactionMirror
 from bokeep.util import ends_with_commit
-
-books = BoKeepBookSet("test_books.conf")
-if books.has_book("test_book_1"):
-    test_book_1 = books.get_book("test_book_1")
-else:
-    test_book_1 = books.add_book("test_book_1")
-
-assert( test_book_1.book_name == "test_book_1" )
 
 class Type1Transaction(Transaction):
     def __init__(self):
@@ -24,77 +16,87 @@ class Type1Transaction(Transaction):
     def append_data(self, append_text):
         self.data += append_text
 
-trans_key = test_book_1.insert_transaction( Type1Transaction() )
+class BoKeepBasicTest(TestCase):
+    def setUp(self):
+        self.books = BoKeepBookSet("tests/test_books.conf")
+        if self.books.has_book("test_book_1"):
+            self.test_book_1 = self.books.get_book("test_book_1")
+        else:
+            self.test_book_1 = self.books.add_book("test_book_1")
 
-@ends_with_commit
-def simple_tests(book, key):
-    trans = book.get_transaction(key)
-    assert( trans.data == "blah" )
-    trans.data = "ha"
-    assert( trans.data == "ha" )
-    trans.reset_data()
-    assert( trans.data == "blah" )
-    trans.append_data(" shit")
-    assert( trans.data == "blah shit" )
+        self.trans_key = self.test_book_1.insert_transaction(
+            Type1Transaction() )
 
-simple_tests(test_book_1, trans_key)
+    def test_interesting_sequence(self):
+        self.assertEquals( self.test_book_1.book_name, "test_book_1" )
 
-@ends_with_commit
-def after_commit_read(book, trans_key):
-    trans = book.get_transaction(trans_key)
-    assert( trans.data == "blah shit" )
+        @ends_with_commit
+        def simple_tests(book, key):
+            trans = book.get_transaction(key)
+            self.assertEquals( trans.data, "blah" )
+            trans.data = "ha"
+            self.assertEquals( trans.data, "ha" )
+            trans.reset_data()
+            self.assertEquals( trans.data, "blah" )
+            trans.append_data(" shit")
+            self.assertEquals( trans.data, "blah shit" )
 
-after_commit_read(test_book_1, trans_key)
+        simple_tests(self.test_book_1, self.trans_key)
 
-@ends_with_commit
-def use_commit_thread(trans_thread, book, trans_key):
-    trans_thread.add_change_tracker_block((book.book_name, trans_key))
-    trans_thread.mod_transaction_attr(
-        (book.book_name, trans_key), 'data', 'fuck')
-    trans_thread.remove_change_tracker((book.book_name, trans_key))
-    trans_thread.add_change_tracker_block((book.book_name, trans_key))
-    trans_thread.remove_change_tracker((book.book_name, trans_key))
+        @ends_with_commit
+        def after_commit_read(book, trans_key):
+            trans = book.get_transaction(trans_key)
+            self.assertEquals( trans.data, "blah shit" )
 
-trans_thread = new_transaction_committing_thread(books)
-use_commit_thread(trans_thread, test_book_1, trans_key)
+        after_commit_read(self.test_book_1, self.trans_key)
 
-trans = test_book_1.get_transaction(trans_key)
-assert( trans.data == "fuck" )
+        @ends_with_commit
+        def use_commit_thread(trans_thread, book, trans_key):
+            trans_thread.add_change_tracker_block((book.book_name, trans_key))
+            trans_thread.mod_transaction_attr(
+                (book.book_name, trans_key), 'data', 'fuck')
+            trans_thread.remove_change_tracker((book.book_name, trans_key))
+            trans_thread.add_change_tracker_block((book.book_name, trans_key))
+            trans_thread.remove_change_tracker((book.book_name, trans_key))
+            
+        trans_thread = new_transaction_committing_thread(self.books)
+        use_commit_thread(trans_thread, self.test_book_1, self.trans_key)
 
-@ends_with_commit
-def use_commit_thread_again(trans_thread, books, book, trans_key):
-    trans_thread.add_change_tracker_block((book.book_name, trans_key))
-    trans_thread.mod_transaction_with_func(
-        (book.book_name, trans_key), 'reset_data', (), {} )
-    trans_thread.mod_transaction_with_func(
-        (book.book_name, trans_key), 'append_data',
-        (' shoot',), {} )
-    trans_thread.remove_change_tracker((book.book_name, trans_key))
-    trans_thread.add_change_tracker_block((book.book_name, trans_key))
-    trans_thread.remove_change_tracker((book.book_name, trans_key))
+        trans = self.test_book_1.get_transaction(self.trans_key)
+        self.assertEquals( trans.data, "fuck" )
 
-use_commit_thread_again(trans_thread, books, test_book_1, trans_key)
+        @ends_with_commit
+        def use_commit_thread_again(trans_thread, books, book, trans_key):
+            trans_thread.add_change_tracker_block((book.book_name, trans_key))
+            trans_thread.mod_transaction_with_func(
+                (book.book_name, trans_key), 'reset_data', (), {} )
+            trans_thread.mod_transaction_with_func(
+                (book.book_name, trans_key), 'append_data',
+                (' shoot',), {} )
+            trans_thread.remove_change_tracker((book.book_name, trans_key))
+            trans_thread.add_change_tracker_block((book.book_name, trans_key))
+            trans_thread.remove_change_tracker((book.book_name, trans_key))
+            
+        use_commit_thread_again(self.trans_thread, self.books,
+                                self.test_book_1, trans_key)
 
-trans = test_book_1.get_transaction(trans_key)
-assert( trans.data == "blah shoot" )
+        trans = self.test_book_1.get_transaction(self.trans_key)
+        self.assertEquals( trans.data, "blah shoot" )
 
-@ends_with_commit
-def use_mirror_class(books, book, trans_key):
-    trans_thread.add_change_tracker_block((book.book_name, trans_key))
-    mirror = TransactionMirror( book.book_name, trans_key, trans_thread)
-    mirror.data = "juice"
-    mirror.append_data(" maker")
-    trans_thread.remove_change_tracker((book.book_name, trans_key))
-    trans_thread.add_change_tracker_block((book.book_name, trans_key))
-    trans_thread.remove_change_tracker((book.book_name, trans_key))
+        @ends_with_commit
+        def use_mirror_class(books, book, trans_key):
+            trans_thread.add_change_tracker_block((book.book_name, trans_key))
+            mirror = TransactionMirror( book.book_name, trans_key, trans_thread)
+            mirror.data = "juice"
+            mirror.append_data(" maker")
+            trans_thread.remove_change_tracker((book.book_name, trans_key))
+            trans_thread.add_change_tracker_block((book.book_name, trans_key))
+            trans_thread.remove_change_tracker((book.book_name, trans_key))
 
-use_mirror_class(books, test_book_1, trans_key)
+        use_mirror_class(self.books, self.test_book_1, self.trans_key)
 
-trans_thread.end_thread_and_join()
+        trans_thread.end_thread_and_join()
 
-trans = test_book_1.get_transaction(trans_key)
-assert( trans.data == "juice maker" )
-
-test_book_1.remove_transaction(trans_key)
-
-
+        trans = self.test_book_1.get_transaction(self.trans_key)
+        self.assertEquals( trans.data, "juice maker" )
+        self.test_book_1.remove_transaction(self.trans_key)
