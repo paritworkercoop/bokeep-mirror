5c61843b62ff4cc8d52c40073dd6b482c481ff71
Mark Jenkins <mark@parit.ca>
2010-05-21 21:09:55 +0000

    created a varient on RobustBackendModule that will take care of session management, the usefulness of this will be demonstrated when the new gnucash backend module is written that uses this
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40510

---
+++ src/bokeep/backend_modules/session_based_robust_backend_module.py
@@ -0,0 +1,28 @@
+from robust_backend_module import RobustBackendModule
+
+#from module import \
+#@    BackendModule, BoKeepBackendException, BoKeepBackendResetException
+
+import transaction
+
+class SessionBasedRobustBackendModule(RobustBackendModule):
+    def flush_backend(self):
+        if not hasattr(self, _v_session_active):
+            self._v_session_active = self.open_session()
+        if not self.can_write():
+            self.close()
+        else:
+            RobustBackendModule.flush_backend(self)
+
+    def close(self, close_reason='reset because close() was called'):
+        RobustBackendModule.close(self, close_reason)
+        if hasattr(self, _v_session_active):
+            del self._v_session_active
+    
+    def open_session(self):
+        raise Exception("robust session based backend modules must implement "
+                        "open_session")
+
+    def can_write(self):
+        return hasattr(self, _v_session_active) and self._v_session_active!=None
+    
