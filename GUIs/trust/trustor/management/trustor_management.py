e7dcc74d0ce91470f8d6c20c8ebd7772b3558e4e
Jamie Campbell <jamie@parit.ca>
2010-08-09 17:06:39 +0000

    interface extended to have basic add, select, and remove capabilities
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40597

--- GUIs/trust/trustor/management/trustor_management.py
+++ GUIs/trust/trustor/management/trustor_management.py
@@ -22,6 +22,8 @@ from bokeep.backend_modules.module import BoKeepBackendException
 
 from GladeWindow import *
 
+from gtk import ListStore, TreeViewColumn, CellRendererText, MessageDialog
+
 #----------------------------------------------------------------------
 
 class trustor_management(GladeWindow):
@@ -35,13 +37,42 @@ class trustor_management(GladeWindow):
         self.backend = self.book.get_backend_module()
         self.trust_module = self.book.get_module('bokeep.modules.trust')
         self.trustors = self.trust_module.get_trustors()
-        print 'current trustors: ' + str(self.trustors)
+        self.current_name = None
 
         ''' '''
         
         self.init()
 
+        self.extended_init()
+
     #----------------------------------------------------------------------
+    def reset_view(self):
+        if self.trustor_view.get_column(0) == None:
+            #no columns to remove
+            return
+        while self.trustor_view.remove_column(self.trustor_view.get_column(0)) > 0:
+            pass
+
+    def refresh_trustor_list(self):
+        
+        self.reset_view()
+
+        self.trustor_list = ListStore( str )
+        self.trustor_view.set_model(self.trustor_list)
+
+        self.trustor_view.append_column(
+            TreeViewColumn('Trustor',CellRendererText(), text=0 ) )
+
+        for trustor in self.trustors:
+            self.trustor_list.append([trustor])
+
+    def extended_init(self):
+        self.add_widgets('trustor_view')
+
+        
+        self.trustor_view = self.widgets['trustor_view']
+
+        self.refresh_trustor_list()
 
     def init(self):
 
@@ -60,21 +91,37 @@ class trustor_management(GladeWindow):
             'on_window_destroy',
             'on_add_button_clicked',
             'on_delete_button_clicked',
+            'on_trustor_view_cursor_changed',
             'on_zoom_button_clicked',
             'on_save_button_clicked',
             ]
 
         top_window = 'TrustManagement'
         GladeWindow.__init__(self, filename, top_window, widget_list, handlers)
+
     #----------------------------------------------------------------------
 
     def on_add_button_clicked(self, *args):
-        print 'add button clicked'
+        self.current_name = None
+        self.widgets['name_entry'].set_text('')
 
     #----------------------------------------------------------------------
 
     def on_delete_button_clicked(self, *args):
-        print 'delete button clicked'
+        for_delete = self.widgets['name_entry'].get_text()
+        self.widgets['name_entry'].set_text('')
+        if self.current_name == None:
+            return
+
+        trustor = self.trust_module.get_trustor(for_delete)
+
+        if len(trustor.transactions) > 0:
+            cantDeleteDia = MessageDialog(flags=gtk.DIALOG_MODAL, message_format='Cannot delete, trustor has associated transacactions.', buttons=gtk.BUTTONS_OK)
+            cantDeleteDia.run()
+            cantDeleteDia.hide()
+        else:            
+            self.trust_module.drop_trustor_by_name(for_delete)
+            self.refresh_trustor_list()
 
     #----------------------------------------------------------------------
 
@@ -84,7 +131,33 @@ class trustor_management(GladeWindow):
     #----------------------------------------------------------------------
 
     def on_save_button_clicked(self, *args):
-        print 'save button clicked'
+        if self.current_name == None:
+            #we're adding someone new
+            trustor_name = self.widgets['name_entry'].get_text()
+            self.current_name = trustor_name
+            self.trust_module.add_trustor_by_name(trustor_name)
+            self.refresh_trustor_list()
+            trustor_name = self.widgets['name_entry'].set_text('')
+        else:
+            #we're updating the name of someone who already exists
+            pass
+
+    #----------------------------------------------------------------------
+
+    def set_trustor(self, trustor_selected):
+        trustor = self.trust_module.get_trustor(trustor_selected)
+
+        self.current_name = trustor.name
+        self.widgets['name_entry'].set_text(trustor.name)
+
+    def on_trustor_view_cursor_changed(self, *args):
+        sel = self.trustor_view.get_selection()
+        sel_iter = sel.get_selected()[1]
+        sel_row = self.trustor_list[sel_iter]
+        trustor_selected = sel_row[0]
+        self.set_trustor(trustor_selected)
+
+
 
 #----------------------------------------------------------------------
 
