73a5e64aee4b29cf34c09c512ce7c94bf2f86e3d
Jamie Campbell <jamie@parit.ca>
2010-08-10 22:05:11 +0000

    some corrections to make the old parrot program run better (de-gnucashification)
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40612

--- parrot-house-money-program/src/transactions.py
+++ parrot-house-money-program/src/transactions.py
@@ -6,8 +6,8 @@ from random import shuffle
 from gtk import ListStore
 
 # python-gnucash imports
-from gnucash import GncNumeric, Split
-from gnucash import Transaction as gnucash_Transaction
+#from gnucash import GncNumeric, Split
+#from gnucash import Transaction as gnucash_Transaction
 
 # this project imports
 from loan_view import LoanView
@@ -44,26 +44,7 @@ class ExchangeTransaction(Transaction):
         self.description = "money exchange"
 
     def save_in_gnucash(self, gnucash_thread):
-        assert( self.debit_account != -1 )
-        debit_account = gnucash_thread.member_accounts[ self.debit_account ]
-        debit_amount = GncNumeric()
-        debit_amount.numerator = self.debit_amount.numerator
-        debit_amount.denominator = self.debit_amount.denominator
-        debit_split = Split( gnucash_thread.gnucash_session.book,
-                             debit_amount, debit_account )
-        credit_account = self.get_gnucash_credit_account(gnucash_thread)
-        credit_amount = GncNumeric()
-        credit_amount.numerator = self.debit_amount.numerator * -1
-        credit_amount.denominator = self.debit_amount.denominator
-        credit_split = Split( gnucash_thread.gnucash_session.book,
-                              credit_amount, credit_account )
-        gnucash_trans = gnucash_Transaction(
-            gnucash_thread.gnucash_session.book,
-            credit_account.commodity,
-            (credit_split, debit_split) )
-
-        self.set_transaction_date(gnucash_trans)
-        gnucash_trans.description = self.description
+        pass
 
 class LoanTransaction(ExchangeTransaction):
     def __init__(self):
@@ -87,62 +68,7 @@ class ShoppingTransaction(Transaction):
         self.item_list = [ list(item) for item in self.item_list ]
 
     def save_in_gnucash(self, gnucash_thread):
-        split_list = []
-        credit_amount = GncNumeric('0')
-
-        paid_by = member_list[self.credit_account][MEMBER_NAME]
-        
-        for line in self.item_list:
-            line_description, line_amount = line[0], line[1]
-            line_amount = GncNumeric(line_amount)
-            
-            # If GST
-            gst = line[3+len(member_list)]
-            pst = line[3+len(member_list)+1]
-            if gst and not pst:
-                line_amount = GncNumeric('1.06') * line_amount
-            elif pst and not gst:
-                line_amount = GncNumeric('1.07') * line_amount
-            elif gst and pst:
-                line_amount = GncNumeric('1.13') * line_amount
-                
-            member_subset_list = [i
-                                  for i in xrange(len(member_list))
-                                  if line[3+i] ]
-            shuffle(member_subset_list)
-            
-            ONE = GncNumeric(1)
-            num_members = GncNumeric( len(member_subset_list) ) + ONE
-            line_splits = []
-            for member in member_subset_list:
-                num_members = num_members - ONE
-                share_of_item = line_amount / num_members
-                split = Split(gnucash_thread.gnucash_session.book,
-                              share_of_item,
-                              gnucash_thread.member_accounts[member] )
-                credit_amount = credit_amount - share_of_item
-                line_splits.append( split )
-                split.memo = "1/%s %s" % (len(member_subset_list),
-                                          line_description)
-                line_amount = line_amount - share_of_item
-                
-            split_list.extend( line_splits )
-
-        split = Split(
-            gnucash_thread.gnucash_session.book,
-            credit_amount,
-            self.get_gnucash_credit_account(gnucash_thread) )
-        split.memo = "paid for"
-        split_list.append(split)
-        
-
-        if len(split_list) > 2:
-            trans = gnucash_Transaction(
-                gnucash_thread.gnucash_session.book,
-                split_list[0].account.commodity,
-                split_list )
-            self.set_transaction_date(trans)
-            trans.description = paid_by + " paid for"
+        pass
     
 # These two lists need to match
 transaction_classes = [ LoanTransaction, ExchangeTransaction,
