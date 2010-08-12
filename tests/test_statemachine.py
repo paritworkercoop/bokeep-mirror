5b049ee7bdcfc888a36476bc9ddcec8abf0cf9c0
Mark Jenkins <mark@parit.ca>
2010-08-12 22:36:47 +0000

    added test showing StateMachineMinChangeDataStore in action
    --HG--
    extra : convert_revision : svn%3A478ed200-3351-0410-a957-86bb2dd1a256/bo-keep/trunk%40613

--- tests/test_statemachine.py
+++ tests/test_statemachine.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 
-from bokeep.util import FunctionAndDataDrivenStateMachine
+from bokeep.util import FunctionAndDataDrivenStateMachine, \
+    StateMachineMinChangeDataStore
 
 # states in the path to the dark side
 NUM_STATES = 4
@@ -40,7 +41,7 @@ class StateMachineTest(TestCase):
     def test_direct_to_darkness(self):
         self.fear = 20
         self.dark_side_path.run_until_steady_state()
-        self.assertEquals( self.dark_side_path.state, SUFFERING )        
+        self.assertEquals( self.dark_side_path.state, SUFFERING )
 
     def test_strait_up_transition_to_darkness(self):
         self.fear = 20
@@ -67,6 +68,44 @@ class StateMachineTest(TestCase):
         self.dark_side_path.advance_state_machine()
         self.assertEquals( self.dark_side_path.state, ANGER )
 
+class StateMachineMinChangeDataStoreTest(TestCase):
+    def setUp(self):
+        dark_side_transitions = (
+            ( (self.enough_fear_to_transition,
+               self.revise_string_and_fear_magnified, ANGER), ), # FEAR
+            ( (self.enough_fear_to_transition,
+               self.revise_string_and_fear_magnified, HATE), ), # ANGER
+            ( (self.enough_fear_to_transition,
+               self.revise_string_and_fear_magnified, SUFFERING), ), # HATE
+            ( (self.enough_fear_to_transition,
+               self.revise_string_and_fear_magnified, SUFFERING), ), # SUFFERING
+            )
+            
+        self.assertEquals(len(dark_side_transitions),  NUM_STATES )
+        self.dark_side_path = FunctionAndDataDrivenStateMachine(
+            transition_table=dark_side_transitions,
+            data=StateMachineMinChangeDataStore(fear_string=None,
+                                                fear_magnified=0,
+                                                yoda_points=12,
+                                                gods_number=3)
+            )
+        self.fear = 0
+
+    def revise_string_and_fear_magnified(self, state_machine, new_state):
+        return state_machine.data.duplicate_and_change(
+            fear_string=DARK_SIDE_STRINGS[new_state],
+            fear_magnified=self.fear * (2 ** new_state))
+
+    def test_fear_magnified(self):
+        self.fear = 20
+        self.assertEquals( self.dark_side_path.state, FEAR )
+        self.dark_side_path.advance_state_machine()
+        self.assertEquals( self.dark_side_path.state, ANGER )
+        self.assertEquals( self.dark_side_path.data.get_value('fear_magnified'),
+                           40)
+
+    def enough_fear_to_transition(self, state_machine, next_state):
+        return self.fear / 5 > state_machine.state
 
 if __name__ == "__main__":
     main()
