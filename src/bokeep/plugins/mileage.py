92b946308a9c30bb5a8451ec53d26adf8df10db1
samuel <none@none>
2011-05-12 16:01:25 -0500

    Instead of using PrototypePlugin, we now use SimplePlugin as a base. Glade is no longer used.  Instead we programmatically create a PyGTK GUI.

--- src/bokeep/plugins/mileage.py
+++ src/bokeep/plugins/mileage.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010  ParIT Worker Co-operative, Ltd <paritinfo@parit.ca>
+# Copyright (C) 2010-2011  ParIT Worker Co-operative, Ltd <paritinfo@parit.ca>
 #
 # This file is part of Bo-Keep.
 #
@@ -15,43 +15,35 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Author: Mark Jenkins <mark@parit.ca>
-
-from persistent import Persistent
-import transaction
+# Authors: Mark Jenkins <mark@parit.ca>
+#          Samuel Pauls <samuel@parit.ca>
 
 from decimal import Decimal, InvalidOperation
 from datetime import datetime, date
-from os.path import abspath, dirname, join, exists
 
-from gtk import RESPONSE_OK
+from gtk import RESPONSE_OK, Label, Dialog, DIALOG_MODAL, STOCK_OK, \
+    STOCK_CANCEL, RESPONSE_CANCEL, Button, Entry, Table, Calendar, HBox
 
 from bokeep.book_transaction import \
-    Transaction, FinancialTransaction, FinancialTransactionLine, \
-    BoKeepTransactionNotMappableToFinancialTransaction
-from bokeep.gui.gladesupport.glade_util import \
-    load_glade_file_get_widgets_and_connect_signals, \
-    do_OldGladeWindowStyleConnect
+    Transaction, FinancialTransaction, FinancialTransactionLine
+from bokeep.simple_trans_editor import SimpleTransactionEditor
+from bokeep.simple_plugin import SimplePlugin
 
-
-ZERO = Decimal(0)
-ONE = Decimal(1)
-NEG_1 = Decimal(-1)
-TWO_PLACES = Decimal('0.01')
-
-def get_mileage_glade_file():
-    import mileage as plugin_mod
-    return join( dirname( abspath( plugin_mod.__file__ ) ),
-                 'mileage.glade')
-
+# One instance for each BoKeep transaction.
+# Contains all the data of the BoKeep transaction and provides it to the
+# back-end plugin.  Member variables are automatically saved.
 class MileageTransaction(Transaction):
     def __init__(self, mileage_plugin):
-        Transaction.__init__(self, mileage_plugin)
-        self.distance = Decimal(ZERO)
+        # Python doesn't automatically initialise the superclass, so we will.
+        super(MileageTransaction,self).__init__(mileage_plugin)
+        
+        # Defaults for the new BoKeep transaction.
+        self.distance = Decimal(0)
         self.trans_date = datetime.today()
 
     def make_trans_line(self, account_spec, negate=False):
-        amount = (self.get_distance() * \
+        TWO_PLACES = Decimal('0.01')
+        amount = (self.distance * \
             self.associated_plugin.distance_multiplier).quantize(
             TWO_PLACES)
         if negate:
@@ -60,188 +52,138 @@ class MileageTransaction(Transaction):
         return_value.account_spec = account_spec
         return return_value
 
+    # Used by the back-end plugin to acquire the financial transactions of this
+    # BoKeep transaction.
     def get_financial_transactions(self):
-        # you should throw BoKeepTransactionNotMappableToFinancialTransaction
-        # under some conditions
-        if isinstance(self.get_distance(), Decimal):
-            return_value = FinancialTransaction(
-                (self.make_trans_line(
-                        self.associated_plugin.get_debit_account() ),
-                 self.make_trans_line(
-                        self.associated_plugin.get_credit_account(), True) )
-                )
-            return_value.description = 'mileage'
-            return_value.trans_date = self.trans_date
-            return_value.currency = self.associated_plugin.get_currency()
-            return (return_value,)
-        else:
-            raise BoKeepTransactionNotMappableToFinancialTransaction(
-                "the number of miles is not specified")
-
-    def get_distance(self):
-        return self.distance
-
-    def set_distance(self, distance):
-        self.distance = distance
-
-def mileage_editable_editor(
-    trans, transid, plugin, gui_parent, change_register_function):
-    editor = mileage_entry(trans, transid, plugin, gui_parent,
-                           change_register_function)
-    return editor
-
-MILEAGE_CODE = 0
-
-def get_plugin_class():
-    return MileagePlugin
-
-class MileagePlugin(Persistent):
-    def __init__(self):
-        self.debit_account = self.credit_account = None
-        self.debit_account_str =  self.credit_account_str = ""
-        self.distance_multiplier = ONE
-        self.currency = self.get_default_currency()
-        self.trans_registry = {}
-
-    def get_default_currency(self):
-        return "CAD"
-
-    def get_debit_account(self):
-        return self.debit_account
-
-    def get_credit_account(self):
-        return self.credit_account   
-
-    def run_configuration_interface(
-        self, parent_window, backend_account_fetch):
-        dia = MileageConfigDialog(parent_window, backend_account_fetch,
-                                  self)
-        dia.run()
-
-    def register_transaction(self, front_end_id, trust_trans):
-        assert( not self.has_transaction(front_end_id) )
-        self.trans_registry[front_end_id] = trust_trans
-
-    def remove_transaction(self, front_end_id):
-        del self.trans_registry[front_end_id]
-
-    def has_transaction(self, trans_id):
-        return trans_id in self.trans_registry
-
-    def get_currency(self):
-        # the currency attribute was added at one point, so we have to
-        # check if its there and set based on default if not
-        if not hasattr(self, "currency"):
-            self.currency = self.get_default_curreny()
-        return self.currency
+        # A financial transaction is composed of several transaction lines
+        # that sum to zero (balance).
+        ft = FinancialTransaction(
+            (self.make_trans_line(self.associated_plugin.debit_account),
+             self.make_trans_line(self.associated_plugin.credit_account, True) )
+            )
+        ft.description = 'Mileage'
+        ft.trans_date = self.trans_date
+        ft.currency = self.associated_plugin.currency
         
-    @staticmethod
-    def get_transaction_type_codes():
-        return (MILEAGE_CODE,)
+        # Return several financial transactions that together represent this
+        # BoKeep transaction.
+        return (ft,)
 
-    @staticmethod
-    def get_transaction_type_from_code(code):
-        assert(code == MILEAGE_CODE)
-        return MileageTransaction
-
-    @staticmethod
-    def get_transaction_type_pulldown_string_from_code(code):
-        assert(code == MILEAGE_CODE)
-        return "Mileage"
-        
-    @staticmethod
-    def get_transaction_edit_interface_hook_from_code(code):
-        return mileage_editable_editor
-
-class mileage_entry(object):
-    def __init__(self, trans, transid, plugin, gui_parent,
-                 change_register_function):
-        self.gui_built = False
-        do_OldGladeWindowStyleConnect(self, get_mileage_glade_file(),
-                                      'window1')
-        self.trans = trans
-        self.widgets['calendar1'].select_month(
+# GUI for editing a BoKeep transaction.
+class MileageEditor(SimpleTransactionEditor):
+    def simple_init_before_show(self):
+        # Setup the calendar.
+        calendar = Calendar()
+        self.mainvbox.pack_start(calendar)
+        calendar.select_month(
             self.trans.trans_date.month-1, self.trans.trans_date.year)
-        self.widgets['calendar1'].select_day(self.trans.trans_date.day)
+        calendar.select_day(self.trans.trans_date.day)
+        calendar.connect('day-selected', self.on_cal_date_changed)
         
-        if isinstance(self.trans.get_distance(), Decimal):
-            self.widgets['amount_entry'].set_text(
-                str(self.trans.get_distance()) )
+        # Setup the distance travelled entry.
+        distance_box = HBox()
+        distance_label = Label('Distance (km/miles):')
+        distance_label.set_alignment(1.0, 0.5) # right, centre
+        distance_box.pack_start(distance_label)
+        distance_entry = Entry()
+        distance_box.pack_end(distance_entry)
+        self.mainvbox.pack_end(distance_box)
+        distance_entry.set_text(str(self.trans.distance))
+        distance_entry.connect('changed', self.on_amount_entry_changed)
 
-        self.plugin = plugin
-        self.change_register_function = change_register_function
-        
-        if not gui_parent == None:
-            self.widgets['vbox1'].reparent(gui_parent)
-        self.top_window.hide()
-        self.gui_built = True
-
-    def detach(self):
-        self.widgets['vbox1'].reparent(self.top_window)
-
-    def on_amount_entry_changed(self, *args):
-        # skip this if being set by code and not user at init
-        if not self.gui_built: return
+    def on_amount_entry_changed(self, widget):
         try:
-            self.trans.set_distance(
-                Decimal(self.widgets['amount_entry'].get_text()))
-        except InvalidOperation: pass
+            self.trans.distance = Decimal(widget.get_text())
+        except InvalidOperation:
+            pass
         else:
             self.change_register_function()
 
-    def on_cal_date_changed(self, *args):
-        # skip this if being set by code and not user at init
-        if not self.gui_built: return
-        (year, month, day) = self.widgets['calendar1'].get_date()
-        self.trans.trans_date = \
-            date(year, month+1, day)
+    def on_cal_date_changed(self, widget):
+        (year, month, day) = widget.get_date()
+        self.trans.trans_date = date(year, month+1, day)
         self.change_register_function()
 
-    
+# GUI for user to change preferences.
+# Preferences are saved to the MileagePlugin.
 class MileageConfigDialog(object):
     def __init__(self, parent_window, backend_account_fetch, plugin):
-        load_glade_file_get_widgets_and_connect_signals(
-            get_mileage_glade_file(), 'dialog1', self, self)
+        self.parent_window = parent_window
         self.backend_account_fetch = backend_account_fetch
         self.plugin = plugin
-        
-        self.set_currency(self.plugin.get_currency())
-        self.set_debit_account(self.plugin.get_debit_account(),
-                               self.plugin.debit_account_str )
-        self.set_credit_account(self.plugin.get_credit_account(),
-                                self.plugin.credit_account_str )
-        if isinstance(self.plugin.distance_multiplier, Decimal):
-            self.distance_multiple_entry.set_text(
-                str(self.plugin.distance_multiplier) )
-                            
-        if parent_window != None:
-            self.dialog1.set_transient_for(parent_window)
-            self.dialog1.set_modal(True)
-
+    
+    # Display the plugin's configuration and save it to the MileagePlugin if the
+    # user clicks OK.
     def run(self):
-        dia_result = self.dialog1.run()
+        self.dia = Dialog('Mileage Configuration',
+             self.parent_window, DIALOG_MODAL,
+             (STOCK_OK, RESPONSE_OK,
+             STOCK_CANCEL, RESPONSE_CANCEL ) )
+        
+        table = Table(4, 2)
+        table.set_row_spacings(4)
+        table.set_col_spacings(4)
+        
+        # Setup the expense account selector.
+        self.expense_account_label = Label()
+        self.expense_account_label.set_alignment(1.0, 0.5) # right, centre
+        table.attach(self.expense_account_label, 0, 1, 0, 1)
+        self.expense_account_button = Button('Select Expense Account')
+        self.set_debit_account(self.plugin.debit_account,
+                               self.plugin.debit_account_str)
+        self.expense_account_button.connect('clicked',
+                                            self.on_select_expense_account)
+        table.attach(self.expense_account_button, 1, 2, 0, 1)
+        
+        # Setup the credit account selector.
+        self.credit_account_label = Label()
+        self.credit_account_label.set_alignment(1.0, 0.5) # right, centre
+        table.attach(self.credit_account_label, 0, 1, 1, 2)
+        self.credit_account_button = Button('Select Credit Account')
+        self.set_credit_account(self.plugin.credit_account,
+                                self.plugin.credit_account_str)
+        self.credit_account_button.connect('clicked',
+                                           self.on_select_credit_account)
+        table.attach(self.credit_account_button, 1, 2, 1, 2)
+        
+        # Setup the distance multiple entry.
+        distance_multiple_label = Label('Multiply distance by:')
+        distance_multiple_label.set_alignment(1.0, 0.5) # right, centre
+        table.attach(distance_multiple_label, 0, 1, 2, 3)
+        self.distance_multiple_entry = Entry()
+        self.distance_multiple_entry.set_text(
+                str(self.plugin.distance_multiplier))
+        table.attach(self.distance_multiple_entry, 1, 2, 2, 3)
+        
+        # Setup the currency entry.
+        currency_label = Label('Currency:')
+        currency_label.set_alignment(1.0, 0.5) # right, centre
+        table.attach(currency_label, 0, 1, 3, 4)
+        self.currency_entry = Entry()
+        self.currency_entry.set_text(self.plugin.currency)
+        table.attach(self.currency_entry, 1, 2, 3, 4)
+        
+        self.dia.vbox.pack_start(table)
+        self.dia.vbox.show_all()
+        dia_result = self.dia.run()
+        
+        # If user clicked OK, update settings.
         if dia_result == RESPONSE_OK:
             self.plugin.debit_account = self.debit_account
             self.plugin.credit_account = self.credit_account
             self.plugin.debit_account_str = self.debit_account_str
             self.plugin.credit_account_str = self.credit_account_str
-            self.plugin.currency = self.get_currency()
+            self.plugin.currency = self.currency_entry.get_text()
             try:
                 self.plugin.distance_multiplier = \
                     Decimal(self.distance_multiple_entry.get_text())
-            except InvalidOperation: pass
-            transaction.get().commit()
-        self.dialog1.destroy()
-    
-    def set_currency(self, currency):
-        self.currency_entry.set_text(currency)
-
-    def get_currency(self):
-        return self.currency_entry.get_text()
+            except InvalidOperation:
+                pass
+        
+        self.dia.destroy()
 
     def handle_account_fetch(self, setter):
-        account_spec, account_str = self.backend_account_fetch(
-            self.dialog1)
+        account_spec, account_str = self.backend_account_fetch(self.dia)
         if account_spec != None:
             setter(account_spec, account_str)
 
@@ -256,8 +198,34 @@ class MileageConfigDialog(object):
         self.credit_account_label.set_text(string)
 
     def on_select_expense_account(self, *args):
-        self.handle_account_fetch(self.set_debit_account )
+        self.handle_account_fetch(self.set_debit_account)
 
     def on_select_credit_account(self, *args):
         self.handle_account_fetch(self.set_credit_account)
 
+# Returns a class that's instantiated once for all entries that this plugin
+# provides.
+def get_plugin_class():
+    return MileagePlugin
+
+# Ties this plugin together and stores the configuration.
+# One instance is instantiated upon first use of this plugin.
+# After that, this class is reloaded.
+class MileagePlugin(SimplePlugin):
+    # This plugin provides a single type of BoKeep entry for logging mileage.
+    # However, another plugin may provide several types of BoKeep entries.
+    ALL_TRANSACTION_TYPES = (MileageTransaction,)
+    DEFAULT_TYPE_STRS = ('Mileage Plugin',)
+    EDIT_INTERFACES = (MileageEditor,)
+    
+    # Class variables are used to store the configuration.
+    debit_account = ('Expenses',)
+    credit_account = ('Assets',)
+    debit_account_str = 'Expenses'
+    credit_account_str = 'Assets'
+    currency = 'CAD'
+    distance_multiplier = Decimal(1)
+    
+    def run_configuration_interface(self, parent_window, backend_account_fetch):
+        dialog = MileageConfigDialog(parent_window, backend_account_fetch, self)
+        dialog.run()
