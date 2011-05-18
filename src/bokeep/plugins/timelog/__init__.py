5ee04fe017b216e8b531f6a87727e8268ba5e89d
Mark Jenkins <mark@parit.ca>
2011-05-18 11:14:42 -0500

    timelog plugin configuration interface now allows selection and retainment of a payroll plugin association

--- src/bokeep/plugins/timelog/__init__.py
+++ src/bokeep/plugins/timelog/__init__.py
@@ -25,7 +25,10 @@ from decimal import Decimal
 from persistent.list import PersistentList
 
 # gtk imports
-from gtk import Label, Dialog, STOCK_OK, RESPONSE_OK, HBox, ComboBox
+from gtk import \
+    Label, Dialog, STOCK_OK, RESPONSE_OK, HBox, ComboBox, ListStore, \
+    combo_box_new_text
+from gobject import TYPE_PYOBJECT
 
 # bokeep imports
 from bokeep.simple_trans_editor import SimpleTransactionEditor
@@ -33,7 +36,7 @@ from bokeep.simple_plugin import SimplePlugin
 from bokeep.book_transaction import \
     Transaction, BoKeepTransactionNotMappableToFinancialTransaction
 from bokeep.gtkutil import \
-    create_editable_type_defined_listview_and_model 
+    create_editable_type_defined_listview_and_model, COMBO_NO_SELECTION
 
 def create_timelog_new_row(timelog_plugin):
     def timelog_new_row():
@@ -59,10 +62,25 @@ class MultiEmployeeTimelogEntry(Transaction):
             """Timelog plugin doesn't put anything directly in backend yet, but """
             """will be picked up by payroll plugin""")
 
+PAYROLL_PLUGIN = "bokeep.plugins.payroll"
+
+PAYROLL_PLUGIN_STR_POSITION, PAYROLL_PLUGIN_STORE_POSITION = range(2)
+
 class TimelogPlugin(SimplePlugin):
     ALL_TRANSACTION_TYPES = (MultiEmployeeTimelogEntry,)
     DEFAULT_TYPE_STRS =("Multi employee timelog entry",)
     EDIT_INTERFACES = (MultiEmployeeTimelogEditor,)
+
+    def __init__(self):
+        SimplePlugin.__init__(self)
+        self.payroll_plugin = None
+
+    def payroll_plugin_selection_combobox_changed(self, combobox, model):
+        if combobox.get_active() == COMBO_NO_SELECTION:
+            self.payroll_plugin = None
+        else:
+            self.payroll_plugin = \
+                model[combobox.get_active()][PAYROLL_PLUGIN_STORE_POSITION]
 
     def run_configuration_interface(
         self, parent_window, backend_account_fetch, book=None):
@@ -78,7 +96,29 @@ class TimelogPlugin(SimplePlugin):
         if book.has_module_enabled("bokeep.plugins.payroll"):
             hbox.pack_start( Label("Pick a payroll plugin instance"),
                              expand=False )
-            payroll_combo = ComboBox()
+            payroll_combo = combo_box_new_text()
+            payroll_liststore = ListStore(str, TYPE_PYOBJECT)
+            payroll_liststore.append( (PAYROLL_PLUGIN,
+                                       book.get_module(PAYROLL_PLUGIN)) )
+            payroll_combo.set_model(payroll_liststore)
+            changed_handler_id = payroll_combo.connect(
+                "changed",
+                self.payroll_plugin_selection_combobox_changed,
+                payroll_liststore)
+            # don't allow the above event handler to be called we set set
+            # the active selection in the combo programatically
+            payroll_combo.handler_block(changed_handler_id)
+            if not hasattr(self, 'payroll_plugin') or \
+                    self.payroll_plugin == None:
+                payroll_combo.set_active(COMBO_NO_SELECTION)
+            else:
+                # once we have support for multiple plugin instances we'll need
+                # to change this
+                payroll_combo.set_active(0)
+            # don't allow the above event handler to be called while we set
+            # the active selection in the combo programatically
+            payroll_combo.handler_unblock(changed_handler_id)
+
             hbox.pack_start(payroll_combo, expand=True)
         else:
             hbox.pack_start(Label("no payroll plugin instance available"))
