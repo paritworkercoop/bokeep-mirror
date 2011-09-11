c12857026289da6f128f34e3057c399b6b20b6e4
Mark Jenkins <mark@parit.ca>
2011-09-11 02:22:09 -0500

    issolated bad problem, Timesheet class not being persistent
    --HG--
    branch : payroll proper use persistent list/dict

--- tests/test_persistence_fun.py
+++ tests/test_persistence_fun.py
@@ -1,24 +1,31 @@
 from unittest import TestCase, main
 from persistent.list import PersistentList
+import transaction
 
 from bokeep.plugins.payroll.canada.timesheet import Timesheet
 
-from test_bokeep_book import BoKeepBasicTestSetup
+from test_bokeep_book import BoKeepBasicTestSetup, \
+    create_filestorage_backed_bookset_from_file
 
-class TestTwoListsOfUnpersistableCrap(BoKeepBasicTestSetup):
+class TestTwoListsOfUnpersistableCrapSetup(BoKeepBasicTestSetup):
     def setUp(self):
         BoKeepBasicTestSetup.setUp(self)
-        self.p_list_1 = PersistentList()
-        self.p_list_2 = PersistentList()
+        p_list_1 = PersistentList()
+        p_list_2 = PersistentList()
         timelog = Timesheet(1, 2, 3)
-        self.p_list_1.append(timelog)
-        self.p_list_2.append(timelog)
+        p_list_1.append(timelog)
+        p_list_2.append(timelog)
         self.books.get_dbhandle().set_sub_database(
-            'haha', (self.p_list_1, self.p_list_2) )
+            'haha', (p_list_1, p_list_2) )
+        self.books.close()
+        self.books_new = create_filestorage_backed_bookset_from_file(
+            self.filestorage_file, False)
 
     def test_same_lists(self):
-        self.assertEquals(self.p_list_1, self.p_list_2)
-        self.assertEquals(self.p_list_1[0], self.p_list_2[0])
+        (p_list_1, p_list_2) = \
+            self.books_new.get_dbhandle().get_sub_database('haha')
+        self.assertEquals(p_list_1, p_list_2)
+        self.assertEquals(p_list_1[0], p_list_2[0])
 
 if __name__ == "__main__":
     main()
